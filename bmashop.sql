fa41f40ec0eb79c5e39b9b32f673aa9563c846a2
BertaBalazsDaniel <bbalazsdaniel@gmail.com>
2025-04-27 18:49:15 +0200

    adatbazis

--- bmashop.sql
+++ bmashop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 27, 2025 at 11:51 AM
+-- Generation Time: Apr 27, 2025 at 11:50 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
